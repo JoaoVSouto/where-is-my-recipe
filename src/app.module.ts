762755f720973020c8ddae29b5dc6890542e704c
João Vítor <joaovsoutosantos@gmail.com>
2021-12-15 17:11:43 -0300

    configure mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_ATLAS_CONN_URL),
+  ],
 })
 export class AppModule {}
